618b53eb8e8bad04fdd157ad13770885100f35b8
Otávio de Castro <otavio.vn01@hotmail.com>
2020-08-01 02:27:41 -0300

    Commit inicial master

---
+++ src/main/java/br/com/erp/sapx/SapxApplication.java
@@ -0,0 +1,13 @@
+package br.com.erp.sapx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/erp/sapx/SapxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erp.sapx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
